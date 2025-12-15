aa2978aee15b072bd57dc698c3656beaa775ca19
Laynie Buten <layniebuten@gmail.com>
2025-12-15 16:56:46 -0500

    Rebuilding App component

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
@@ -7,20 +8,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
+app.get('/', (req: Request, res: Response) => {
+  return res
+    .status(200)
+    .sendFile(path.resolve(import.meta.dirname, 'index.html'));
+});
 
 // 404 error handler
 app.use((req: Request, res: Response) => {
-  return res.status(404).send('Page not found.');
+  return res.sendStatus(404).send('Page not found.');
 });
 
 // global error handler 500
 interface Error {
-    err: any;
+  err: any;
 }
 
-app.use((err: Error, req: Request, res:Response) => {
-    return res.json(err);
-})
+app.use((err: Error, req: Request, res: Response) => {
+  return res.json(err);
+});
 
 // Initialization
 app.listen(PORT, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -7,61 +7,75 @@ import { AddExpense } from './pages/addExpense';
 import { Expenses } from './pages/expenses';
 import { Balances } from './pages/balances';
 
-const FirstPage = () => {
-  return (
-    <div>
-      <div>
-        <Form />
-        <div className="flex justify-center">
-          <Button
-            className="flex items-center gap-2 bg-[#3A7FE5] text-white px-4 py-2 rounded font-bold"
-            name="Start New Adventure"
-            variant="ho"
-            isActive={true}
-            onClick={() => console.log('Button Click Success!')}
-            plusIcon={<Plus />}
-            route="/create-adventure"
-          />
-        </div>
-        <h2 className="font-semibold text-4 pl-3 mt-4 mb-4">All Adventures</h2>
-        <div>
-          <div className="flex flex-col justify-center items-center gap-3">
-            <List title="Trip to Paris" subtitle="4 members" amount={1300} />
-            <List title="Trip to Tokyo" subtitle="3 members" amount={2000} />
-            <List
-              title="Trip to Cartagena"
-              subtitle="7 members"
-              amount={4000}
-            />
-            <List
-              title="Trip to Islamabad"
-              subtitle="5 members"
-              amount={1800}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/create-adventure" element={<CreateNewGroup />} />
-          <Route path="/" element={<FirstPage />} />
-          <Route path="/trip-name" element={<GroupTripDetails />}>
-            <Route index element={<Navigate to="expenses" replace />} />
-            <Route path="expenses" element={<Expenses />} />
-            <Route path="balances" element={<Balances />} />
-          </Route>
-          <Route path="/adventure-details" element={<AddExpense />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div id="title">
+        <h1>WalletWise</h1>
+      </div>
+      
+      
+    </>
   );
 }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div>
+//         <Form />
+//         <div>
+//         {/* <div className="flex justify-center"> */}
+//           <Button
+//             className="flex items-center gap-2 bg-[#3A7FE5] text-white px-4 py-2 rounded font-bold"
+//             name="Start New Adventure"
+//             variant="ho"
+//             isActive={true}
+//             onClick={() => console.log('Button Click Success!')}
+//             // plusIcon={<Plus />}
+//             route="/create-adventure"
+//           />
+//         </div>
+//         <h2 className="font-semibold text-4 pl-3 mt-4 mb-4">All Adventures</h2>
+//         <div>
+//           <div className="flex flex-col justify-center items-center gap-3">
+//             <List title="Trip to Paris" subtitle="4 members" amount={1300} />
+//             <List title="Trip to Tokyo" subtitle="3 members" amount={2000} />
+//             <List
+//               title="Trip to Cartagena"
+//               subtitle="7 members"
+//               amount={4000}
+//             />
+//             <List
+//               title="Trip to Islamabad"
+//               subtitle="5 members"
+//               amount={1800}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/create-adventure" element={<CreateNewGroup />} />
+//           <Route path="/" element={<FirstPage />} />
+//           <Route path="/trip-name" element={<GroupTripDetails />}>
+//             <Route index element={<Navigate to="expenses" replace />} />
+//             <Route path="expenses" element={<Expenses />} />
+//             <Route path="balances" element={<Balances />} />
+//           </Route>
+//           <Route path="/adventure-details" element={<AddExpense />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
 
 export default App;
